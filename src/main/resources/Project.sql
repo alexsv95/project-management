3a374a39f5304d89d4078f62ce4dc97f6099ca40
alexsv95 <a.sosnov95@gmail.com>
2019-06-19 03:16:01 +0400

    Edit icons

---
+++ src/main/resources/Project.sql
@@ -0,0 +1,4 @@
+insert into projects (id, name, description, start_date, status, note, created, attachmentPath)
+values (1, 'Отделка и ремонт квартиры', 'Адрес: Казань, ул. Пушкина, д 12А, кв 38. Необходим демонтаж старой отделки, штукатурка стен и поклеить обоями',
+        '', 'PROGRESS', 'Cтены под покраску + комбинация с обоями'),
+       (2, 'REGISTERED', 'Косметический ремонт', '', '')
